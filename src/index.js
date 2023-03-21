c71be898262c28e447c2a8e67e765622e47d1442
0kira-vgl <matheusgtiburcio@gmail.com>
2023-03-21 20:13:48 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Calculator from './main/Calculator';
 import reportWebVitals from './reportWebVitals';
-
+//Teste
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
